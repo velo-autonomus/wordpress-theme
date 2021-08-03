7bb2d9dd130353a8417d5a98152c61ba6b056e73
Moritz <moritz.ahrens2@gmail.com>
2021-08-03 13:55:21 +0200

    Rolling out the bulldozers

--- footer.php
+++
@@ -1,9 +0,0 @@
-        <footer>
-            <span><?php echo get_theme_mod('copyright_owner');?> © <?php echo get_theme_mod('copyright_year');?></span>
-            <?php
-            wp_nav_menu(array('menu' => 'footer'));
-            ?>
-        </footer>
-        <?php wp_footer(); ?>
-    </body>
-<html>
